ac7427cac2ac28a3f87fa81d05c707d67ff03f4d
avairama <bharanimurugan1@gmail.com>
2024-05-29 22:56:14 +0530

    initial changes

---
+++ src/basic/Helloworld.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Helloworld {
+
+	public Helloworld() {
+		System.out.println("Hellow world, Welcome to DevOps training in edureka");
+	}
+
+}
---
+++ src/basic/Test.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Test {
+
+public static void main(String[] args) {
+
+		System.out.println("I'm the Test");
+		
+	}
+
+}
