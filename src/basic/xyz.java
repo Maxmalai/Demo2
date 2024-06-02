bc4d520b221dfb7bf0ac3573ef267aeb6ce35ebe
avairama <bharanimurugan1@gmail.com>
2024-06-02 21:12:56 +0530

    modified test file print message

---
+++ src/basic/xyz.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class xyz {
+
+	public xyz() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
